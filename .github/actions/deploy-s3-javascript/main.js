b5465db2680a9fe92b2c59de1a1cd7e5a3bc06f8
towficaziz <tapolly01@gmail.com>
2023-06-10 00:23:44 +0200

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,8 @@ function run(){
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!');
+    const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);  // ::set-output
 }
 
 run();
